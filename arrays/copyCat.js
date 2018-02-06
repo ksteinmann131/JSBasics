41771550320800f4f02672837ba30f5204fea4da
Kelsey Steinmann <kelseysteinmann@Kelseys-MacBook-Pro.local>
2018-02-06 15:45:29 -0700

    completed copyCat

--- arrays/copyCat.js
+++ arrays/copyCat.js
@@ -1,7 +1,10 @@
 // Create a function called copyCat that takes in an array, copies, and
 // returns the second and third index values.
 
-var copyCat;
+function copyCat(arr){
+  var copycatArray = arr.slice(2,4)
+  return copycatArray
+};
 
 // DO NOT DELETE
 module.exports = copyCat;

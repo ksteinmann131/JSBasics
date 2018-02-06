2947860ee84f967615a4f3eb5b03f141406fe919
Kelsey Steinmann <kelseysteinmann@Kelseys-MacBook-Pro.local>
2018-02-06 15:09:45 -0700

    addEnd complete

--- arrays/addEnd.js
+++ arrays/addEnd.js
@@ -1,7 +1,11 @@
 // Create a function called addEnd that takes in an array and a number and
 // positions the number at the end of the array. Return the new array.
 
-var addEnd;
+function addEnd(array, num) {
+  array.push(num);
+  return array;
+};
+
 
 // DO NOT DELETE
 module.exports = addEnd;
--- arrays/addFront.js
+++ arrays/addFront.js
@@ -1,7 +1,10 @@
 // Create a function called addFront that takes in an array and a number and
 // positions the number at the front of the array. Return the new array.
 
-var addFront;
+function addFront(array, num){
+  array.unshift(num);
+  return array;
+};
 
 // DO NOT DELETE
 module.exports = addFront;

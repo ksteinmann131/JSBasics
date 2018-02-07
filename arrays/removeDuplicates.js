77f64e69cf0c5f72d2535ad80e34a27770a6850c
Kelsey Steinmann <kelseysteinmann@Kelseys-MacBook-Pro.local>
2018-02-07 10:44:55 -0700

    completed removeDuplicates

--- arrays/removeDuplicates.js
+++ arrays/removeDuplicates.js
@@ -1,6 +1,22 @@
 // Write a JavaScript function removeDuplicates that removes duplicate values fom an array.
 
-var removeDuplicates;
+function removeDuplicates(arr){
+  var noDubsArray =[];
+  for (i = 0; i < arr.length; i ++) {
+    if (noDubsArray.indexOf(arr[i]) == -1){
+      noDubsArray.push(arr[i])
+    }
+  }
+  return noDubsArray;
+};
 
 // DO NOT DELETE
 module.exports = removeDuplicates;
+
+// for(let i = 0;i < arr.length; i++){
+//     if(unique_array.indexOf(arr[i]) == -1){
+//         unique_array.push(arr[i])
+//     }
+// }
+// return unique_array
+// }

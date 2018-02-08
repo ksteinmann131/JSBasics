bcc7af9c375ee482a144f52455fe74b061be5f07
Kelsey Steinmann <kelseysteinmann@Kelseys-MacBook-Pro.local>
2018-02-08 10:48:56 -0700

    completed sumIt

--- arrays/sumIt.js
+++ arrays/sumIt.js
@@ -1,7 +1,12 @@
 // Create a function called sumIt that takes in the array and returns the sum
 // of each of the values in the array.
 
-var sumIt;
+function sumIt(arr){
+  var newArr = arr.reduce(function(a,b){
+    return a + b;
+  }); 
+  return newArr;
+};
 
 // DO NOT DELETE
 module.exports = sumIt;
